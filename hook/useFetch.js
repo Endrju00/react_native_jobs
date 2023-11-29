27e22809e6914cf3bb5ab51abbcce0ab6a6b735f
Endrju00 <andrzej.kapczynski21@gmail.com>
2023-11-29 17:51:49 +0100

    Add fetching jobs data

---
+++ hook/useFetch.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        params: { ...query },
+        headers: {
+            'X-RapidAPI-Key': '0c0db01db9msh7eb335e4a30a408p1903ffjsn25ba97595c60',
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        }
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options);
+            setData(response.data.data);
+        } catch (error) {
+            setError(error);
+            alert('Something went wrong, please try again later.');
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    };
+
+    return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
