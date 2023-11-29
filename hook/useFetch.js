d9ea1bd865ce97956a1631300725df41e01fb681
Endrju00 <andrzej.kapczynski21@gmail.com>
2023-11-29 18:10:36 +0100

    Update useFetch.js

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -11,7 +11,7 @@ const useFetch = (endpoint, query) => {
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         params: { ...query },
         headers: {
-            'X-RapidAPI-Key': '0c0db01db9msh7eb335e4a30a408p1903ffjsn25ba97595c60',
+            'X-RapidAPI-Key': '',
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         }
     };
